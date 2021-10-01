d2d20532ea8eb686844299b9d3158f1ac45602d2
ThiagoLR <thiaguinhoLrodrigues007@gmail.com>
2021-09-30 22:14:05 -0300

    Acesso ao Cmsp

---
+++ AutomacaoCmsp.py
@@ -0,0 +1,41 @@
+import pyautogui
+import time
+
+pyautogui.alert("Deixa comigo")
+pyautogui.press('winleft')
+time.sleep(1)
+pyautogui.write('google chrome')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(3)
+pyautogui.write('https://cmspweb.ip.tv/')
+pyautogui.PAUSE = 0.5
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.moveTo(679, 436)
+pyautogui.PAUSE = 1
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+pyautogui.PAUSE = 0.5
+pyautogui.moveTo(753, 365)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+pyautogui.press('3')
+pyautogui.moveTo(636, 438)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+pyautogui.write("0qjpxd5x")
+pyautogui.press('enter')
+pyautogui.PAUSE = 2
+pyautogui.moveTo(1092, 96)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+pyautogui.PAUSE = 0.8
+pyautogui.moveTo(844, 192)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+pyautogui.PAUSE = 0.8
+pyautogui.moveTo(1065, 101)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+time.sleep(5)
